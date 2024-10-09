f784f10b2ffa288a11fda7127036b79bb339e0c9
Nick <nicholas.t.bui@gmail.com>
2024-10-09 16:44:23 -0500

    Started basic layout of todo

--- src/app/todolist/todolist.component.ts
+++ src/app/todolist/todolist.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-todolist',
   standalone: true,
-  imports: [],
+  imports: [MatInputModule, MatIconModule, MatButtonModule],
   templateUrl: './todolist.component.html',
   styleUrl: './todolist.component.css'
 })
